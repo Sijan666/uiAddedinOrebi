7608498509aea562075315473e4f618531f9cdb7
Sijan666 <666majharulislam@gmail.com>
2025-12-24 01:28:53 +0600

    after banner change

--- src/components/layouts/AfterBanner.jsx
+++ src/components/layouts/AfterBanner.jsx
@@ -1,15 +1,49 @@
-import Container from "../Container"
-import Images from "../Images"
-import afterBanner from '../../assets/afterBanner.png'
+import { FaTruck } from "react-icons/fa";
+import { FaUndoAlt } from "react-icons/fa";
+import Flex from "../Flex";
+import Container from "../Container";
 
 const AfterBanner = () => {
     return (
-        <>
-            <div className="">
-                <Images imgSrc={afterBanner} className={'w-[70%] mx-auto'}/>
-            </div>
-        </>
-    )
-}
+    <>
+        <div className="py-[22px] border-b border-b-[#F0F0F0] border-t border-t-[#F0F0F0] -mt-2">
+            <Container>
+                <Flex className={"justify-between"}>
+                    <div className="">
+                        <Flex>
+                            <h4 className="text-[26px] font-dmFont font-black text-black">
+                            2
+                            </h4>
+                            <h5 className="font-dmFont font-normal text-[16px] text-[#6D6D6D] pl-[17px]">
+                            Two years warranty
+                            </h5>
+                        </Flex>
+                    </div>
+                    <div className="">
+                        <Flex>
+                            <h4 className="text-[26px] font-dmFont font-black text-black">
+                            <FaTruck />
+                            </h4>
+                            <h5 className="font-dmFont font-normal text-[16px] text-[#6D6D6D] pl-[17px]">
+                            Free shipping
+                            </h5>
+                        </Flex>
+                    </div>
+                    <div className="">
+                        <Flex>
+                            <h4 className="text-[26px] font-dmFont font-black text-black">
+                            <FaUndoAlt />
+                            </h4>
+                            <h5 className="font-dmFont font-normal text-[16px] text-[#6D6D6D] pl-[17px]">
+                            Return policy in 30 days
+                            </h5>
+                        </Flex>
+                    </div>
+                </Flex>
+            </Container>
+        </div>
+    </>
+    );
+};
 
-export default AfterBanner
+export default AfterBanner;
