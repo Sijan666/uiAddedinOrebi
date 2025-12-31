eeb3de857fa5749b08d911133138de36dc8fba88
Sijan666 <666majharulislam@gmail.com>
2025-12-31 18:25:12 +0600

    some changes

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -50,21 +50,69 @@ const Shop = () => {
                 <div className="sideBar w-[30%] pb-[30px]">
                     <div className="category">
                         <h4 className='text-[#262626] font-bold text-[20px] pb-[30px]'>Shop By Category</h4>
-                        <p data-filter="all" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black'>All Products</p>
-                        <p data-filter=".catOne" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black'>Category 1</p>
-                        <p data-filter=".catTwo" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black'>Category 2</p>
-                        <p data-filter=".catThree" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black'>Category 3</p>
-                        <p data-filter=".catFour" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black'>Category 4</p>
-                        <p data-filter=".catFive" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black'>Category 5</p>
+                        <p data-filter="all" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>All Products</p>
+                        <p data-filter=".catOne" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Category 1</p>
+                        <p data-filter=".catTwo" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Category 2</p>
+                        <p data-filter=".catThree" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Category 3</p>
+                        <p data-filter=".catFour" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Category 4</p>
+                        <p data-filter=".catFive" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Category 5</p>
+                    </div>
+                    <div className="color py-12">
+                        <h4 className='text-[#262626] font-bold text-[20px] pb-[30px]'>Shop by Color</h4>
+                        <p data-filter="all" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>All Color</p>
+                        <p data-filter=".catOne" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Color 1</p>
+                        <p data-filter=".catTwo" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Color 2</p>
+                        <p data-filter=".catThree" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Color 3</p>
+                        <p data-filter=".catFour" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Color 4</p>
+                        <p data-filter=".catFive" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Color 5</p>
+                    </div>
+                    <div className="brand py-12">
+                        <h4 className='text-[#262626] font-bold text-[20px] pb-[30px]'>Shop by Brand</h4>
+                        <p data-filter="all" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>All Brand</p>
+                        <p data-filter=".catOne" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Brand 1</p>
+                        <p data-filter=".catTwo" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Brand 2</p>
+                        <p data-filter=".catThree" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Brand 3</p>
+                        <p data-filter=".catFour" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Brand 4</p>
+                        <p data-filter=".catFive" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>Brand 5</p>
+                    </div>
+                    <div className="price py-12">
+                        <h4 className='text-[#262626] font-bold text-[20px] pb-[30px]'>Shop by Price</h4>
+                        <p data-filter="all" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>All Price</p>
+                        <p data-filter=".catOne" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>$0.00 - $9.99</p>
+                        <p data-filter=".catTwo" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>$10.00 - $19.99</p>
+                        <p data-filter=".catThree" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>$20.00 - $29.99</p>
+                        <p data-filter=".catFour" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>$30.00 - $39.99</p>
+                        <p data-filter=".catFive" className='text-[#767676] text-base border-b border-[#F0F0F0] pb-[22px] my-[30px] cursor-pointer hover:font-bold hover:text-black duration-300'>$40.00 - $69.99</p>
                     </div>
                 </div>
                 <div className="w-[70%]">
-                    <div className="firstLine flex gap-x-2 pb-[50px]">
-                        <IoGrid />
-                        <CiGrid2H />
+                    <div className="firstLine flex justify-between pb-[50px]">
+                        <div className='flex gap-x-3'>
+                            <IoGrid className='text-[20px]'/>
+                            <CiGrid2H className='text-[20px]'/>
+                        </div>
+                        <div className="flex justify-between gap-x-5">
+                            <div className="flex gap-x-3 items-center ">
+                                <p className='text-[#767676] text-base'>Sort by:</p>
+                                <select className='px-4 py-1 text-[#767676] border border-[#F0F0F0] outline-none w-40'>
+                                    <option value="">Featured</option>
+                                    <option value="">Best Sellers</option>
+                                    <option value="">New Arrivals</option>
+                                    <option value="">Final Offer</option>
+                                </select>
+                            </div>
+                            <div className="flex gap-x-3 items-center ">
+                                <p className='text-[#767676] text-base'>Shows:</p>
+                                <select className='px-4 py-1 text-[#767676] border border-[#F0F0F0] outline-none w-20'>
+                                    <option value="">12</option>
+                                    <option value="">24</option>
+                                    <option value="">36</option>
+                                    <option value="">48</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     {/* <div className=""> */}
-                        {/* Ref ekhane add kora hoyeche jate mixitup ei container ke chinte pare */}
                         <div className="pt-8 pb-20 w-full" ref={containerRef}>
                             <Flex className={'flex-wrap gap-x-4 gap-y-10'}>
                                 {allData.map((item)=>(
